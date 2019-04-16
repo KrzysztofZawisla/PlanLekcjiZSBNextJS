2b49a8814303e3054ebce5d2d5fb320032488f16
KrzysztofZawisla <32432944+KrzysztofZawisla@users.noreply.github.com>
2019-04-16 21:26:20 +0200

    Code optimization

--- app-src/pages/components/button.js
+++ app-src/pages/components/button.js
@@ -1,11 +1,13 @@
+import React from "react";
 import Link from "next/link";
 
-const Button = (props) => {
-  const states = {
-    buttonText: props.buttonText == "" || props.buttonText == undefined ? "Powrót na stronę główną" : props.buttonText,
-    buttonLink: props.buttonLink == "" || props.buttonLink == undefined ? "/" : props.buttonLink,
-    buttonLinkAs: props.buttonLinkAs == "" || props.buttonLinkAs == undefined ? null : props.buttonLinkAs,
-    buttonColor: () => {
+export default class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.buttonText = props.buttonText == "" || props.buttonText == undefined ? "Powrót na stronę główną" : props.buttonText;
+    this.buttonLink = props.buttonLink == "" || props.buttonLink == undefined ? "/" : props.buttonLink;
+    this.buttonLinkAs = props.buttonLinkAs == "" || props.buttonLinkAs == undefined ? null : props.buttonLinkAs;
+    this.buttonColor = () => {
       if(props.buttonColor == "" || props.buttonColor == undefined) {
         return "#e05416"
       }
@@ -20,29 +22,34 @@ const Button = (props) => {
           return "#e05416";
       }
     }
-  };
-  return(
-    <Link prefetch href={ states.buttonLink } as={ states.buttonLinkAs }>
-      <div className="Button">
-        <style jsx>{`
-          .Button {
-            width: 100%;
-            margin 0 auto 15px auto;
-            background: ${ states.buttonColor() };
-            padding: 5px;
-            text-align: center;
-            border-radius: 5px;
-            font-size: 24px;
-            color: #fff;
-          }
-          .Button:last-child {
-            margin-top: 0;
-          }
-        `}</style>
-        { states.buttonText }
-      </div>
-    </Link>
-  );
+    this.buttonPrefetch = false;
+  }
+  componentDidMount() {
+    this.buttonPrefetch = true;
+    this.forceUpdate();
+  }
+  render() {
+    return(
+      <Link prefetch={ this.buttonPrefetch } href={ this.buttonLink } as={ this.buttonLinkAs }>
+        <div className="Button">
+          <style jsx>{`
+            .Button {
+              width: 100%;
+              margin 0 auto 15px auto;
+              background: ${ this.buttonColor() };
+              padding: 5px;
+              text-align: center;
+              border-radius: 5px;
+              font-size: 24px;
+              color: #fff;
+            }
+            .Button:last-child {
+              margin-top: 0;
+            }
+          `}</style>
+          { this.buttonText }
+        </div>
+      </Link>
+    );
+  }
 }
-
-export default Button;
