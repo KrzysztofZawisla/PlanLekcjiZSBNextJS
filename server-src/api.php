6d385419b2361c81f53f5c9c26fec6080576dda0
KrzysztofZawisla <32432944+KrzysztofZawisla@users.noreply.github.com>
2019-09-30 23:13:23 +0200

    Rewrite to typescript with better code

--- server-src/api.php
+++ server-src/api.php
@@ -1,8 +1,37 @@
 <?php
+  // connected lesson plans with links to zsbrybnik
+  $link_association = array(
+    "1ba" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o16.html",
+    "1wa" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o17.html",
+    "1ga" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o18.html",
+    "1ia" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o19.html",
+    "1ka" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o20.html",
+    "1ba8" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o21.html",
+    "1aw8" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o22.html",
+    "1ga8" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o23.html",
+    "1ia8" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o24.html",
+    "1w8" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o25.html",
+    "2w" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o15.html",
+    "2wa" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o14.html",
+    "2ib" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o13.html",
+    "2ia" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o12.html",
+    "2ga" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o11.html",
+    "2ba" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o10.html",
+    "3gi" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o9.html",
+    "3ib" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o8.html",
+    "3ia" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o7.html",
+    "3ba" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o6.html",
+    "4gi" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o5.html",
+    "4ib" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o4.html",
+    "4ia" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o3.html",
+    "4bb" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o2.html",
+    "4ba" => "https://zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o1.html",
+  );
+
   require_once("simple_html_dom.php");
   header("Access-Control-Allow-Origin: *");
   header("Content-Type: text/html; charset=UTF-8");
-  $tableStyles =
+  $table_styles =
   "<style>
   .tabela {
     box-sizing: border-box;
@@ -18,6 +47,7 @@
   .tabela th, td {
     padding: 2.5px;
     width: auto;
+    height: 50px;
     text-align: center;
     border: .5px solid #e05416;
   }
@@ -28,58 +58,27 @@
     cursor: default;
   }
   </style>";
+  $html = "";
   $request_method = $_SERVER["REQUEST_METHOD"];
   if($request_method == "GET") {
-    if(isset($_GET["klasa"]) && $_GET["klasa"] != "") {
-      $klasa = $_GET["klasa"];
-      if($klasa == "1ba") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o15.html");
-      } elseif($klasa == "1ga") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o16.html");
-      } elseif($klasa == "1ia") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o17.html");
-      } elseif($klasa == "1ib") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o18.html");
-      } elseif($klasa == "1wa") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o19.html");
-      } elseif($klasa == "1w") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o20.html");
-      } elseif($klasa == "2ia") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o12.html");
-      } elseif($klasa == "2ib") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o13.html");
-      } elseif($klasa == "2ba") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o11.html");
-      } elseif($klasa == "2gi") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o14.html");
-      } elseif($klasa == "3ba") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o5.html");
-      } elseif($klasa == "3bb") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o6.html");
-      } elseif($klasa == "3ia") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o7.html");
-      } elseif($klasa == "3ib") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o8.html");
-      } elseif($klasa == "3gi") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o9.html");
-      } elseif($klasa == "3w") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o10.html");
-      } elseif($klasa == "4ba") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o1.html");
-      } elseif($klasa == "4bb") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o2.html");
-      } elseif($klasa == "4ga") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o3.html");
-      } elseif($klasa == "4ia") {
-        $html = file_get_html("http://www.zsbrybnik.pl/pliki/plany/plan_lekcji/plany/o4.html");
+    if(isset($_GET["class"]) && $_GET["class"] != "") {
+      $class = $_GET["class"];
+      foreach($link_association as $key => $link) {
+        if($key == $class) {
+          $html = file_get_html($link);
+        }
       }
-      foreach($html->find('a') as $element) {
-        $element->href = "#";
+      if($html != "") {
+        foreach ($html->find('a') as $element) {
+          $element->href = "#";
+        }
+        $htmlReturn = $html->find("table", 1)->find("tr", 0);
+        echo $htmlReturn.$table_styles;
+      } else {
+        echo "Nie odnaleziono planu lekcji";
       }
-      $htmlReturn = $html->find("table", 1)->find("tr", 0);
-      echo $htmlReturn.$tableStyles;
-    } else if($_GET["klasa"] == "") {
-      echo "Błędne zapytanie";
+    } else if($html == "") {
+      echo "Nie odnaleziono planu lekcji";
     }
   }
 ?>
